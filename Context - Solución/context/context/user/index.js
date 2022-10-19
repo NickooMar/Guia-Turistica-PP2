92b7f4a919ab60add4b6c1245ed096d055d2a225
Nicolás <nicoo.marsili@gmail.com>
2022-10-19 17:21:15 -0300

    Set User in Home (Backend Request)

--- Context - Solución/context/context/user/index.js
+++
@@ -1,9 +0,0 @@
-// modules
-import {
-  createContext
-} from 'react';
-
-// custom context
-const UserContext = createContext(null);
-
-export default UserContext;
--- Context - Solución/context/helpers/index.js
+++
@@ -1,8 +0,0 @@
-// Ajax
-import ajax from "../utils/axios.utils";
-// Fetching Profile
-export const fetchProfile = async () => {
-  const { data: response } = await ajax.get("/api/me/profile");
-
-  return response.data;
-};
